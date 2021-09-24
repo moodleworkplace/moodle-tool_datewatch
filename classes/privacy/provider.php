b949948eb26db9ddfc9043221f78f94fb667bf09
Marina Glancy <marina@moodle.com>
2021-09-24 19:16:58 +0200

    Privacy implementation

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of the tool_datewatch plugin for Moodle - http://moodle.org/
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -14,14 +14,24 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace tool_datewatch\privacy;
+
 /**
- * tool_datewatch
+ * Privacy Subsystem implementation for tool_datewatch.
  *
  * @package    tool_datewatch
- * @copyright  2016 Marina Glancy
+ * @copyright  2021 Marina Glancy
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+class provider implements \core_privacy\local\metadata\null_provider {
 
-$string['pluginname'] = 'Date watcher';
-$string['privacy:metadata'] = 'The Date watcher plugin does not store any personal data.';
-$string['taskname'] = 'Watch dates in the database and notify watchers';
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function get_reason() : string {
+        return 'privacy:metadata';
+    }
+}

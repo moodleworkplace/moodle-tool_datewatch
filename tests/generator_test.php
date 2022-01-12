9c681b1fb512b90a485019095bd7552578d84078
Marina Glancy <marina@moodle.com>
2022-01-12 23:40:22 +0100

    WP-3101 comply with codechecker v3.0.5

--- tests/generator_test.php
+++ tests/generator_test.php
@@ -14,13 +14,18 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace tool_datewatch;
+
+use advanced_testcase;
+use tool_datewatch_generator;
+
 /**
  * Class generator_test
  *
  * @package     tool_datewatch
  * @copyright   2021 Marina Glancy
  */
-class tool_datewatch_generator_testcase extends advanced_testcase {
+class generator_test extends advanced_testcase {
 
     /**
      * Get dynamic rule generator
@@ -41,12 +46,12 @@ class tool_datewatch_generator_testcase extends advanced_testcase {
     public function test_register_watchers() {
         global $DB;
         $this->resetAfterTest();
-        (new tool_datewatch\task\watch())->execute();
+        (new \tool_datewatch\task\watch())->execute();
         $this->get_generator()->register_watcher('course');
-        (new tool_datewatch\task\watch())->execute();
+        (new \tool_datewatch\task\watch())->execute();
         $this->assertCount(1, $DB->get_records('tool_datewatch', ['tablename' => 'course', 'fieldname' => 'startdate']));
         $this->get_generator()->register_watcher('user_enrolments');
-        (new tool_datewatch\task\watch())->execute();
+        (new \tool_datewatch\task\watch())->execute();
         $this->assertCount(1, $DB->get_records('tool_datewatch', ['tablename' => 'course', 'fieldname' => 'startdate']));
         $this->assertCount(1, $DB->get_records('tool_datewatch', ['tablename' => 'user_enrolments', 'fieldname' => 'timeend']));
     }
